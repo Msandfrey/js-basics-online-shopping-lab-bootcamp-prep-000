c17fb70afe4fc917788266afcc7cea72371532c1
Matthew Sandfrey <msandfrey@sbcglobal.net>
2017-10-04 23:06:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,17 +11,37 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
- var newItem = {item: Math.ceil(Math.random()*100)}
+ var newItem = {[item]: Math.ceil(Math.random()*100)}
  if(cart.push(newItem)) console.log(`${item} has been added to your cart.`)
  return cart
 }
 
 function viewCart() {
   // write your code here
+  var string = "In your cart, you have "
+  if(cart.length === 0) {
+    console.log("Your shopping cart is empty")
+    return
+  }
+  for(var i = 0; i<cart.length;i++){
+    if(i != 0 && cart.length === 2) string += " and "
+    else if(i != 0)string += ", and "
+    var keys = Object.keys(cart[i])
+    string+= `${keys[0]} at $${cart[keys[0]]}`
+  }
+  string += "."
+  console.log(string)
 }
 
 function total() {
   // write your code here
+  var cost = 0
+  for(var i = 0; i < cart.length; i++){
+    var item = cart[i]
+    var key = Object.keys(item)
+    cost += cart[key[0]]
+  }
+  return cost
 }
 
 function removeFromCart(item) {
