db59f5165fdef853df959eb3d08e2a75c4b4bd9f
Matthew Sandfrey <msandfrey@sbcglobal.net>
2017-10-04 23:56:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function viewCart() {
   }else{
     for(var i = 0; i<cart.length;i++){
       if(i != 0 && cart.length === 2) {string += " and"}
-      else if(i != 0 && cart.length != 1){string += ", and"}
+      else if(i != 0){string += ", and"}
       var keys = Object.keys(cart[i])
       var key = keys[0]
       var price = cart[i][key]
