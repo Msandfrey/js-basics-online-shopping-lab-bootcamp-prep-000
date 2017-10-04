133021d443e7e47bf0812647c57205d229a632ee
Matthew Sandfrey <msandfrey@sbcglobal.net>
2017-10-04 23:37:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function viewCart() {
     console.log("Your shopping cart is empty.")
   }else{
     for(var i = 0; i<cart.length;i++){
-      if(i != 0 && cart.length === 2) {string += " and "} 
+      if(i != 0 && cart.length === 2) {string += " and "}
       else if(i != 0){string += ", and "}
       var keys = Object.keys(cart[i])
       var key = keys[0]
