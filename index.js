842702c385801529427a31c2325225407d35243c
Matthew Sandfrey <msandfrey@sbcglobal.net>
2017-10-05 00:01:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ function viewCart() {
     console.log("Your shopping cart is empty.")
   }else{
     for(var i = 0; i<cart.length;i++){
-      if(i != 0 && cart.length === 2) {string += " and"}
-      else if(i != 0){string += ", and"}
+      if(i != 0 && cart.length != 2) {string += ", "}
+      if(i != 0){string += " and"}
       var keys = Object.keys(cart[i])
       var key = keys[0]
       var price = cart[i][key]
@@ -54,7 +54,3 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   // write your code here
 }
-addToCart("lemons")
-addToCart("mango")
-addToCart("fruit")
-viewCart()
